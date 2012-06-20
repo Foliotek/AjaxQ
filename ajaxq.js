1c88e9f0aded12cb842372dd0c03b41a28f1799a
bgrins <briangrinstead@gmail.com>
2012-06-20 07:54:34 -0500

    fix jshint error (options is already defined)

--- ajaxq.js
+++ ajaxq.js
@@ -23,11 +23,11 @@
         promise.complete = promise.always;
 
         // Create a deep copy of the arguments, and enqueue this request.
-        var opts = $.extend(true, {}, opts);
+        var clonedOptions = $.extend(true, {}, opts);
         enqueue(function() {
 
             // Send off the ajax request now that the item has been removed from the queue
-            var jqXHR = $.ajax.apply(window, [opts]).always(dequeue);
+            var jqXHR = $.ajax.apply(window, [clonedOptions]).always(dequeue);
 
             // Notify the returned deferred object with the correct context when the jqXHR is done or fails
             // Note that 'always' will automatically be fired once one of these are called: http://api.jquery.com/category/deferred-object/.
