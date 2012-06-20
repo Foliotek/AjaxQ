6a58dc717af22fa8a984c0670aef7650378885a0
bgrins <briangrinstead@gmail.com>
2012-06-20 07:52:30 -0500

    trim whitespace

--- ajaxq.js
+++ ajaxq.js
@@ -2,33 +2,33 @@
 // Copyright (c) 2012 Foliotek Inc.
 // MIT License
 // https://github.com/Foliotek/ajaxq
- 
+
 (function($) {
 
     var queues = {};
-    
+
     // Register an $.ajaxq function, which follows the $.ajax interface, but allows a queue name which will force only one request per queue to fire.
     $.ajaxq = function(qname, opts) {
-    
+
         if (typeof opts === "undefined") {
             throw ("AjaxQ: queue name is not provided");
         }
-        
-        // Will return a Deferred promise object extended with success/error/callback, so that this function matches the interface of $.ajax  
+
+        // Will return a Deferred promise object extended with success/error/callback, so that this function matches the interface of $.ajax
         var deferred = $.Deferred(),
             promise = deferred.promise();
-        
+
         promise.success = promise.done;
         promise.error = promise.fail;
         promise.complete = promise.always;
-        
+
         // Create a deep copy of the arguments, and enqueue this request.
         var opts = $.extend(true, {}, opts);
         enqueue(function() {
-        
+
             // Send off the ajax request now that the item has been removed from the queue
             var jqXHR = $.ajax.apply(window, [opts]).always(dequeue);
-            
+
             // Notify the returned deferred object with the correct context when the jqXHR is done or fails
             // Note that 'always' will automatically be fired once one of these are called: http://api.jquery.com/category/deferred-object/.
             jqXHR.done(function() {
@@ -38,9 +38,9 @@
                 deferred.reject.apply(this, arguments);
             });
         });
-        
+
         return promise;
-        
+
         // If there is no queue, create an empty one and instantly process this item.
         // Otherwise, just add this item onto it for later processing.
         function enqueue(cb) {
@@ -65,12 +65,12 @@
             }
         }
     };
-    
+
     // Register a $.postq and $.getq method to provide shortcuts for $.get and $.post
     // Copied from jQuery source to make sure the functions share the same defaults as $.get and $.post.
     $.each( [ "getq", "postq" ], function( i, method ) {
         $[ method ] = function( qname, url, data, callback, type ) {
-        
+
             if ( $.isFunction( data ) ) {
                 type = type || callback;
                 callback = data;
@@ -95,5 +95,5 @@
         }
         return false;
     };
-    
+
 })(jQuery);
